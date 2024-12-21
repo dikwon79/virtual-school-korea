ed4e146e44d5572b78a2b54428ba0f6bcff331a0
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-12-22 06:28:28 +0900

    zod?

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,33 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { handleForm } from "./ations";
+import { useActionState } from "react";
 
 export default function Login() {
+  const [state, action] = useActionState(handleForm, {
+    potato: 1,
+  } as any);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요 !</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={action} className="flex flex-col gap-3">
+        <FormInput name="email" type="email" placeholder="Email" required />
 
         <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
-          errors={[]}
         />
 
-        <FormButton loading={false} text="Create account" />
+        <FormButton text="Create account" />
       </form>
 
       <SocialLogin />
