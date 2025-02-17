7503dcf02f3bd060a8577bd56a2c41e51e1fa45d
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-02-16 23:27:38 -0800

    streaming

---
+++ app/(tabs)/streams/add/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { useFormState } from "react-dom";
+import { startStream } from "./actions";
+import { useActionState } from "react";
+export default function AddStream() {
+  const [state, action] = useActionState(startStream, null);
+  return (
+    <form className="p-24 flex flex-col gap-2" action={action}>
+      <Input
+        name="title"
+        required
+        placeholder="Title or your stream."
+        errors={state?.formErrors}
+      />
+      <Button text="Start streaming" />
+    </form>
+  );
+}
