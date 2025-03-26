ed7b08aa8afb15c58932fa8e248e314134781a8c
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-03-26 12:16:53 -0700

    why

--- lib/db.ts
+++ lib/db.ts
@@ -4,14 +4,11 @@ import { PrismaClient } from "@prisma/client";
 
 // export default db;
 
-// PrismaClient를 한 번만 생성해 재사용하기 위한 싱글톤 설정
 let db: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  // Production 환경에서는 PrismaClient를 단 한 번만 생성
   db = new PrismaClient();
 } else {
-  // Development 환경에서는 Hot Reload를 고려하여 글로벌 변수로 관리
   if (!global.db) {
     global.db = new PrismaClient();
   }
