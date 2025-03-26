fe6a95a08e46345680de7c266ee7099ce1fd0856
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-03-26 16:23:12 -0700

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,8 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-export const db = globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // 디버깅을 위해 로그 활성화
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default db;
+export default prisma;
