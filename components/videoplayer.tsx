59ff79914079bb41ccc2ea5626e3fafb81605d4a
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-03-27 11:22:51 -0700

    webrtc player

--- components/videoplayer.tsx
+++ components/videoplayer.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React, { useEffect, useRef, useState } from "react";
-import WHEPClient from "../lib/whepClient";
+import { WebRTCPlayer } from "@eyevinn/webrtc-player";
 
 interface VideoPlayerProps {
   webRtcUrl: string;
@@ -17,40 +18,31 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ webRtcUrl }) => {
   };
 
   useEffect(() => {
-    let client: WHEPClient | null = null;
+    let player: WebRTCPlayer | null = null;
 
-    const loadWHEPClient = async () => {
-      const videoElement = videoRef.current;
-
-      if (videoElement && webRtcUrl) {
-        client = new WHEPClient(webRtcUrl, videoElement);
-        videoElement.muted = isMuted; // 초기 음소거 상태 설정
-        await videoElement.play().catch((error) => {
-          console.error("비디오 자동 재생 실패:", error);
+    const initializePlayer = async () => {
+      if (videoRef.current) {
+        player = new WebRTCPlayer({
+          video: videoRef.current,
+          type: "whep",
         });
 
-        const mediaStream = videoElement.srcObject;
-        console.log("mediaStream:", mediaStream);
-
-        // mediaStream이 MediaStream 타입인지 확인
-        if (mediaStream instanceof MediaStream) {
-          const audioTracks = mediaStream.getAudioTracks();
-          if (audioTracks.length > 0) {
-            console.log("오디오 트랙이 있습니다:", audioTracks[0]);
-          } else {
-            console.log("오디오 트랙이 없습니다.");
+        try {
+          await player.load(new URL(webRtcUrl));
+          if (!isMuted) {
+            player.unmute();
           }
-        } else {
-          console.log("스트림은 MediaStream이 아닙니다.");
+        } catch (error) {
+          console.error("Failed to load WebRTC stream:", error);
         }
       }
     };
 
-    loadWHEPClient();
+    initializePlayer();
 
     return () => {
-      if (client) {
-        client.close();
+      if (player) {
+        player.unload();
       }
     };
   }, [webRtcUrl, isMuted]);
@@ -61,13 +53,13 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ webRtcUrl }) => {
         ref={videoRef}
         autoPlay
         playsInline
-        muted={isMuted} // 초기 음소거 상태 설정
+        muted={isMuted}
         width="400"
         height="400"
         className="border border-gray-300 rounded-lg"
       />
       <button
-        className="text-white bg-slate-600 hover:bg-slate-700 py-2 px-4 mt-3 rounded-lg shadow-lg flex justify-center items-center transition-all duration-200 ease-in-out"
+        className="text-white bg-slate-600 hover:bg-slate-700 py-2 px-4 mt-3 rounded-lg shadow-lg transition-all duration-200 ease-in-out"
         onClick={toggleMute}
       >
         {isMuted ? "소리 켜기" : "소리 끄기"}
