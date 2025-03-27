bced2c6179833819f8588f9ae34b5941fd785148
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-03-26 21:41:44 -0700

    audio problem

--- components/videoplayer.tsx
+++ components/videoplayer.tsx
@@ -1,56 +1,59 @@
-"use client";
-import React, { useEffect, useRef } from "react";
-import WHEPClient from "../lib/whepClient"; // WHEPClient가 export된 경우
+import React, { useEffect, useRef, useState } from "react";
+import WHEPClient from "../lib/whepClient";
+
 interface VideoPlayerProps {
   webRtcUrl: string;
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ webRtcUrl }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const unmuteVideo = () => {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const toggleMute = () => {
     if (videoRef.current) {
-      videoRef.current.muted = false; // 음소거 해제
+      videoRef.current.muted = !videoRef.current.muted;
+      setIsMuted(videoRef.current.muted);
     }
-    console.log(videoRef);
   };
+
   useEffect(() => {
-    let client: WHEPClient | null = null; // 타입 명시
+    let client: WHEPClient | null = null;
 
     const loadWHEPClient = async () => {
-      // Dynamically import WHEPClient
-      //const { default: WHEPClient } = await import("../lib/whepClient");
       const videoElement = videoRef.current;
 
       if (videoElement && webRtcUrl) {
-        // Initialize the WHEPClient
         client = new WHEPClient(webRtcUrl, videoElement);
+        videoElement.muted = isMuted; // 초기 음소거 상태 설정
+        await videoElement.play().catch((error) => {
+          console.error("비디오 자동 재생 실패:", error);
+        });
       }
     };
 
     loadWHEPClient();
 
-    // Cleanup when component unmounts
     return () => {
       if (client) {
         client.close();
       }
     };
-  }, [webRtcUrl]);
+  }, [webRtcUrl, isMuted]);
 
   return (
     <div className="flex flex-col items-center">
-      {/* Video element for WebRTC stream */}
       <video
         ref={videoRef}
         autoPlay
         playsInline
+        muted={isMuted} // 초기 음소거 상태 설정
         className="border border-gray-300 rounded-lg"
-      ></video>
+      />
       <button
         className="text-white bg-slate-600 hover:bg-slate-700 py-2 px-4 mt-3 rounded-lg shadow-lg flex justify-center items-center transition-all duration-200 ease-in-out"
-        onClick={unmuteVideo}
+        onClick={toggleMute}
       >
-        소리 켜기
+        {isMuted ? "소리 켜기" : "소리 끄기"}
       </button>
     </div>
   );
